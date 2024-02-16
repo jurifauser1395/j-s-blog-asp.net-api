4b09f45e34908e71861bff0bfd6ce89c519f5838
jurifauser1395 <jurifauser@pm.me>
2024-02-16 14:57:54 +0100

    init project

---
+++ j-s-blog-asp.net-api/Models/Domain/BlogPost.cs
@@ -0,0 +1,23 @@
+﻿namespace j_s_blog_asp.net_api.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Content { get; set; }
+
+        public string FeaturedImageUrl { get; set; }
+
+        public string UrlHandle { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public string Author { get; set; }
+
+        public bool IsVisibe { get; set; }
+    }
+}
---
+++ j-s-blog-asp.net-api/Models/Domain/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace j_s_blog_asp.net_api.Models.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string UrlHandle { get; set; }
+    }
+}
